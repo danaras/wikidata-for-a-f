77adb4bbb34bcc56674d84eeb15d4ccdcc57aa48
Danara Sarioglu <ds4489@nyu.edu>
2018-01-21 21:04:14 -0500

    now outputing to a csv file

--- parse-wikidata.py
+++ parse-wikidata.py
@@ -3,16 +3,18 @@ import os, json, csv
 title =''
 language =''
 qid=''
-p21and106 = []
-
+entitiesFound = False
+output = open('output.csv', 'w')
+csvWriter = csv.writer(output)
+csvWriter.writerow(['language','title','p21','gender','p106','occupation'])
 with open('test.csv','rb') as csvfile:
 	reader = csv.reader(csvfile)
 	for row in reader:
 		info = list(row)
 		langTitle = info[0]
 		language = langTitle.split(':')[0]
-		title = langTitle.split(':')[1]
-		title = title.replace(' ', '+')
+		titleOriginal = langTitle.split(':')[1]
+		title = titleOriginal.replace(' ', '+')
 		print language
 		print title
 
@@ -24,33 +26,55 @@ with open('test.csv','rb') as csvfile:
 			jsonData = json.loads(wikiData)
 			try:
 				keys = jsonData["entities"].keys()
+				entitiesFound = True
 			except:
 				print "cannot find entities"
+				entitiesFound = False
 			for key in keys:
 				qid = key
 			print qid
 			try:
-				p21and106.append(jsonData["entities"][qid]["claims"]["P21"][0]["mainsnak"]["datavalue"]["value"]["id"])
+				p21 = (jsonData["entities"][qid]["claims"]["P21"][0]["mainsnak"]["datavalue"]["value"]["id"])
 				print jsonData["entities"][qid]["claims"]["P21"][0]["mainsnak"]["datavalue"]["value"]["id"]
 			except:
 				print "no p21"
 			try:
-				p21and106.append(jsonData["entities"][qid]["claims"]["P106"][0]["mainsnak"]["datavalue"]["value"]["id"])
+				p106 = (jsonData["entities"][qid]["claims"]["P106"][0]["mainsnak"]["datavalue"]["value"]["id"])
 				print jsonData["entities"][qid]["claims"]["P106"][0]["mainsnak"]["datavalue"]["value"]["id"]
 			except:
 				print "no p106"
-			for x in p21and106:
-				pRequest = Request('https://www.wikidata.org/w/api.php?action=wbgetentities&ids='+x+'&props=descriptions%7Clabels&languages=en%7Cde%7Cfr&format=json')
+			if p21:
+				pRequest = Request('https://www.wikidata.org/w/api.php?action=wbgetentities&ids='+p21+'&props=descriptions%7Clabels&languages=en%7Cde%7Cfr&format=json')
 				try:
 					pResponse = urlopen(pRequest)
 					pData = pResponse.read()
 					pJsonData = json.loads(pData)
 					# print pJsonData
 					# print pJsonData["entities"][x]["labels"]["en"]["value"]
-					gender = pJsonData["entities"][x]["labels"]["en"]["value"]
+					gender = pJsonData["entities"][p21]["labels"]["en"]["value"]
 
+					print gender
 				except URLError, e:
 				    print 'No kittez. Got an error code:', e
-			p21and106 = []
+			if p106:
+				pRequest = Request('https://www.wikidata.org/w/api.php?action=wbgetentities&ids='+p106+'&props=descriptions%7Clabels&languages=en%7Cde%7Cfr&format=json')
+				try:
+					pResponse = urlopen(pRequest)
+					pData = pResponse.read()
+					pJsonData = json.loads(pData)
+					# print pJsonData
+					# print pJsonData["entities"][x]["labels"]["en"]["value"]
+					occupation = pJsonData["entities"][p106]["labels"]["en"]["value"]
+
+					print occupation
+				except URLError, e:
+					print 'No kittez. Got an error code:', e
+			csvWriter.writerow([language, titleOriginal, p21, gender, p106, occupation])
+			p21 = ''
+			p106 = ''
+			language = ''
+			titleOriginal = ''
+			gender = ''
+			occupation = ''
 		except URLError, e:
 		    print 'General error. Got an error code:', e
